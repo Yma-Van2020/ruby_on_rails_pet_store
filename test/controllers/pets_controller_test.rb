e8b9ff0ffedc77a7d153194befced3eaeab42f95
Yucong Ma <candicema2018@gmail.com>
2025-04-14 15:00:46 -0700

    added test cases for model and controller

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,23 +1,92 @@
 require "test_helper"
 
 class PetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get pets_index_url
-    assert_response :success
+  setup do
+    @pet = pets(:one)
   end
 
-  test "should get show" do
-    get pets_show_url
+  test "should get index" do
+    get pets_url
     assert_response :success
   end
 
   test "should get new" do
-    get pets_new_url
+    get new_pet_url
+    assert_response :success
+  end
+
+  test "should create pet with valid attributes" do
+    assert_difference("Pet.count") do
+      post pets_url, params: {
+        pet: {
+          name: "Snowy",
+          age: "2",
+          description: "White and fluffy doggo"
+        }
+      }
+    end
+
+    assert_redirected_to pet_url(Pet.last)
+  end
+
+  test "should not create pet with invalid attributes" do
+    assert_no_difference("Pet.count") do
+      post pets_url, params: {
+        pet: {
+          name: "",               # invalid: name can't be blank
+          age: "3",
+          description: "Short"    # invalid: description too short
+        }
+      }
+    end
+
+    assert_response :success
+    assert_select "div#error_explanation"
+  end
+
+  test "should show pet" do
+    get pet_url(@pet)
     assert_response :success
   end
 
   test "should get edit" do
-    get pets_edit_url
+    get edit_pet_url(@pet)
     assert_response :success
+  end
+
+  test "should update pet with valid attributes" do
+    patch pet_url(@pet), params: {
+      pet: {
+        name: "Updated Name",
+        age: "4",
+        description: "Updated and valid description"
+      }
+    }
+
+    assert_redirected_to pet_url(@pet)
+    @pet.reload
+    assert_equal "Updated Name", @pet.name
+  end
+
+  test "should not update pet with invalid attributes" do
+    patch pet_url(@pet), params: {
+      pet: {
+        name: "",
+        description: "Too short"
+      }
+    }
+
+    assert_response :success
+    assert_select "div#error_explanation"
+    @pet.reload
+    refute_equal "", @pet.name
+  end
+
+  test "should destroy pet" do
+    assert_difference("Pet.count", -1) do
+      delete pet_url(@pet)
+    end
+
+    assert_redirected_to pets_url
   end
 end
--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    pet = Pet.new(name: "Buddy", description: "Loyal and friendly dog")
+    assert pet.valid?
+  end
+
+  test "is invalid without a name" do
+    pet = Pet.new(description: "Loyal and friendly dog")
+    assert_not pet.valid?
+    assert_includes pet.errors[:name], "can't be blank"
+  end
+
+  test "is invalid with short description" do
+    pet = Pet.new(name: "Buddy", description: "Short")
+    assert_not pet.valid?
+    assert_includes pet.errors[:description], "is too short (minimum is 10 characters)"
+  end
 end
